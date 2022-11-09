f2efc42e6ec14fcc6f60204f594b280f3a555dff
David Teo <starrynightskyhoney@gmail.com>
2022-11-09 19:32:48 +0530

    day-3 submit

--- expedia-clone/src/App.js
+++ expedia-clone/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
+import SimpleCard from "./Components/SimpleCard";
+import HomePage from './Pages/HomePage'
+import Map from './Components/Map'
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+
+  </div>;
 }
 
 export default App;
