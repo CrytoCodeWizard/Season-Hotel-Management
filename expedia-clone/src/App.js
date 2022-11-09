bd72f4636de1982b83b148e88643b2a1c28a322a
David Teo <starrynightskyhoney@gmail.com>
2022-11-09 20:07:22 +0530

    rohitday-2

--- expedia-clone/src/App.js
+++ expedia-clone/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 
-import Map from './Components/Map'
 function App() {
   return <div className="App">
 
--- expedia-clone/src/index.js
+++ expedia-clone/src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <ChakraProvider>
-        <App />
-    </ChakraProvider>
+    <App />
 );
 
 
