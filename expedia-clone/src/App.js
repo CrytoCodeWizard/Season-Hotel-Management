edf21317a859110c13f9044dec52e20ecd0a7461
David Teo <starrynightskyhoney@gmail.com>
2022-11-10 10:13:07 +0530

    day2
    singlepage

--- expedia-clone/src/App.js
+++ expedia-clone/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
+import SingleProduct from "./Pages/SingleProduct";
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
---
+++ expedia-clone/src/Components/Rooms.jsx
@@ -0,0 +1,21 @@
+import { Box, Grid, GridItem, Text } from '@chakra-ui/react';
+import React from 'react'
+
+const Rooms = () => {
+  return (
+    <Box w="100%">
+      <Box textAlign="initial">
+        <Text fontSize="2rem" fontWeight={500} lineHeight="1.5rem">
+          Choose your room
+        </Text>
+      </Box>
+      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+              <GridItem w="100%" bg="blue.500" border="red solid 1px">
+                  
+        </GridItem>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Rooms
