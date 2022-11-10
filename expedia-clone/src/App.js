5d6f350477122cbc71c3b1a527cf53e4283545f4
David Teo <starrynightskyhoney@gmail.com>
2022-11-10 12:04:48 +0530

    Nabar + homepage added

--- expedia-clone/src/App.js
+++ expedia-clone/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-
-
+import NavBar from './Components/navbar'
+import HomePage from './Pages/HomePage'
 
 function App() {
   return <div className="App">
-
+    <NavBar />
+    <HomePage />
   </div>;
 }
 
