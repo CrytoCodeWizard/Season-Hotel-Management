441881261e6ba97f805d49765c617a121bac2ef5
David Teo <starrynightskyhoney@gmail.com>
2022-11-10 19:24:01 +0530

    Footer and HomePage modification

--- expedia-clone/src/App.js
+++ expedia-clone/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import NavBar from './Components/navbar'
-import HomePage from './Pages/HomePage'
+import Footer from "./Components/Footer";
+import NavBar from "./Components/navbar";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-  return <div className="App">
-    <NavBar />
-    <HomePage />
-  </div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <HomePage />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
