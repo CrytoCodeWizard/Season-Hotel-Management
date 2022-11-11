3c5afa8d425b937c2aaa832c9d8481e674f00a61
David Teo <starrynightskyhoney@gmail.com>
2022-11-11 23:54:26 +0530

    SignUp

--- expedia-clone/src/Redux/store.js
+++ expedia-clone/src/Redux/store.js
@@ -1,4 +1,18 @@
-import { applyMiddleware, legacy_createStore } from "redux";
-import { reducer } from "./AppContext/reducer";
+import {
+  applyMiddleware,
+  legacy_createStore,
+  combineReducers,
+  compose,
+} from "redux";
 import thunk from "redux-thunk";
-export const store = legacy_createStore(reducer, applyMiddleware(thunk));
+import { reducer } from "./AppContext/reducer";
+import { reducer as AuthReducer } from "./AuthContext/reducer";
+
+const rootRuducer = combineReducers({ reducer, AuthReducer });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = legacy_createStore(
+  rootRuducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
